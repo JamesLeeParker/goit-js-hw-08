7f68bceab72b18d60d05d184f1bd1efba6615bee
Ivan Kostenko <89205729+JamesLeeParker@users.noreply.github.com>
2021-11-15 19:28:31 +0200

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,13 @@
+import Player from '@vimeo/player';
+const iframeRef = document.querySelector('#vimeo-player');
+const player = new Vimeo.Player(iframeRef);
+const throttle = require('lodash.throttle');
 
+const currentTime = localStorage.getItem('videoplayer-current-time');
+console.log(currentTime);
+
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('timeupdate', onPlay);
